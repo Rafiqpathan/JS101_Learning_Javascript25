d7915f40c0f7b28f7d82c252224e06bd81b58f42
unknown <mohrafiq900@gmail.com>
2022-12-28 17:23:32 +0530

    new 2

---
+++ Javascript-IV-Conditional-Statements-IW-2/p1.js
@@ -0,0 +1,4 @@
+let N=9;
+if(N%3==0){
+  console.log("multiple of 3");
+}
---
+++ Javascript-IV-Conditional-Statements-IW-2/p4.js
@@ -0,0 +1,15 @@
+let db_user="abc@gmail.com";
+let db_pass="123456";
+
+let user="abc@gmail.com";
+let pass="123456";
+
+if(db_user==user){
+  if(db_pass==pass){
+    console.log(" user can login")
+  }else{
+    console.log(" user can not login")
+  }
+}else {
+  console.log("wrong details");
+}
